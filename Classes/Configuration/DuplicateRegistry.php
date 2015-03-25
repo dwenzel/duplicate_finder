3a993780cde931059be512fd13ca948f9bc8395b
Dirk Wenzel <wenzel@cps-it.de>
2015-03-25 11:14:57 +0100

    [TASK] DuplicateRegistry implemented

---
+++ Classes/Configuration/DuplicateRegistry.php
@@ -0,0 +1,302 @@
+<?php
+namespace CPSIT\DuplicateFinder\Configuration;
+
+use TYPO3\CMS\Core\SingletonInterface;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\ArrayUtility;
+
+class DuplicateRegistry implements SingletonInterface {
+
+	/**
+	 * @var array
+	 */
+	protected $registry = array();
+
+	/**
+	 * @var array
+	 */
+	protected $extensions = array();
+
+	/**
+	 * @var string
+	 */
+	protected $sqlTemplate = '';
+
+	/**
+	 * @var array
+	 */
+	protected $addedDataQualityTabs = array();
+
+	/**
+	 * Returns a class instance
+	 *
+	 * @return \CPSIT\DuplicateFinder\Configuration\DuplicateRegistry
+	 */
+	static public function getInstance() {
+		return GeneralUtility::makeInstance(__CLASS__);
+	}
+
+
+	/**
+	 * Creates this object.
+	 */
+	public function __construct() {
+		$this->sqlTemplate = str_repeat(PHP_EOL, 3) . 'CREATE TABLE %s (' . PHP_EOL
+			. '  %s int(11) DEFAULT \'0\' NOT NULL' . PHP_EOL . ');' . str_repeat(PHP_EOL, 3);
+	}
+
+	/**
+	 * Adds a new duplicate configuration to this registry.
+	 * TCA changes are directly applied
+	 *
+	 * @param string $extensionKey Extension key to be used
+	 * @param string $tableName Name of the table to be registered
+	 * @param string $fieldName Name of the field to be registered
+	 * @param array $options Additional configuration options
+	 *              + fieldList: field configuration to be added to showitems
+	 *              + typesList: list of types that shall visualize the duplicate field
+	 *              + position: insert position of the duplicate field
+	 *              + label: backend label of the duplicate field
+	 *              + fieldConfiguration: TCA field config array to override defaults
+	 * @return boolean
+	 * @throws \InvalidArgumentException
+	 * @throws \RuntimeException
+	 */
+	public function add($extensionKey, $tableName, $fieldName = 'isDuplicate', array $options = array()) {
+		$didRegister = FALSE;
+		if (empty($tableName) || !is_string($tableName)) {
+			throw new \InvalidArgumentException('No or invalid table name "' . $tableName . '" given.', 1427275047
+			);
+		}
+		if (empty($extensionKey) || !is_string($extensionKey)) {
+			throw new \InvalidArgumentException('No or invalid extension key "' . $extensionKey . '" given.', 1427275072
+			);
+		}
+
+		if (!$this->isRegistered($tableName, $fieldName)) {
+			$this->registry[$tableName][$fieldName] = $options;
+			$this->extensions[$extensionKey][$tableName][$fieldName] = $fieldName;
+
+			if (!isset($GLOBALS['TCA'][$tableName]['columns']) && isset($GLOBALS['TCA'][$tableName]['ctrl']['dynamicConfigFile'])) {
+				// Handle deprecated old style dynamic TCA column loading.
+				ExtensionManagementUtility::loadNewTcaColumnsConfigFiles();
+			}
+
+			if (isset($GLOBALS['TCA'][$tableName]['columns'])) {
+				$this->applyTcaForTableAndField($tableName, $fieldName);
+				$didRegister = TRUE;
+			}
+		}
+
+		return $didRegister;
+	}
+
+	/**
+	 * Gets all extension keys that registered a duplicate configuration.
+	 *
+	 * @return array
+	 */
+	public function getExtensionKeys() {
+		return array_keys($this->extensions);
+	}
+
+	/**
+	 * Gets all duplicate aware tables
+	 *
+	 * @return array
+	 */
+	public function getCategorizedTables() {
+		return array_keys($this->registry);
+	}
+
+
+	/**
+	 * Tells whether a table has a duplicate configuration in the registry.
+	 *
+	 * @param string $tableName Name of the table to be looked up
+	 * @param string $fieldName Name of the field to be looked up
+	 * @return boolean
+	 */
+	public function isRegistered($tableName, $fieldName = 'isDuplicate') {
+		return isset($this->registry[$tableName][$fieldName]);
+	}
+
+	/**
+	 * Generates tables definitions for all registered tables.
+	 *
+	 * @return string
+	 */
+	public function getDatabaseTableDefinitions() {
+		$sql = '';
+		foreach ($this->getExtensionKeys() as $extensionKey) {
+			$sql .= $this->getDatabaseTableDefinition($extensionKey);
+		}
+
+		return $sql;
+	}
+
+	/**
+	 * Generates table definitions for registered tables by an extension.
+	 *
+	 * @param string $extensionKey Extension key to have the database definitions created for
+	 * @return string
+	 */
+	public function getDatabaseTableDefinition($extensionKey) {
+		if (!isset($this->extensions[$extensionKey]) || !is_array($this->extensions[$extensionKey])) {
+			return '';
+		}
+		$sql = '';
+
+		foreach ($this->extensions[$extensionKey] as $tableName => $fields) {
+			foreach ($fields as $fieldName) {
+				$sql .= sprintf($this->sqlTemplate, $tableName, $fieldName);
+			}
+		}
+
+		return $sql;
+	}
+
+	/**
+	 * Applies the additions directly to the TCA
+	 *
+	 * @param string $tableName
+	 * @param string $fieldName
+	 */
+	protected function applyTcaForTableAndField($tableName, $fieldName) {
+		$this->addTcaColumn($tableName, $fieldName, $this->registry[$tableName][$fieldName]);
+		$this->addToAllTcaTypes($tableName, $fieldName, $this->registry[$tableName][$fieldName]);
+	}
+
+	/**
+	 * Add a new TCA Column
+	 *
+	 * @param string $tableName Name of the table to be duplicate aware
+	 * @param string $fieldName Name of the field to be used to store duplicate status
+	 * @param array $options Additional configuration options
+	 *              + fieldConfiguration: TCA field config array to override defaults
+	 *              + label: backend label of the categories field
+	 * @return void
+	 */
+	protected function addTcaColumn($tableName, $fieldName, array $options) {
+		// Makes sure to add more TCA to an existing structure
+		if (isset($GLOBALS['TCA'][$tableName]['columns'])) {
+			// Take specific label into account
+			$label = 'LLL:EXT:duplicate_finder/Resources/Private/Language/locallang_tca.xlf:label.isDuplicate';
+			if (!empty($options['label'])) {
+				$label = $options['label'];
+			}
+
+			// Take specific value of exclude flag into account
+			$exclude = TRUE;
+			if (isset($options['exclude'])) {
+				$exclude = (bool) $options['exclude'];
+			}
+
+			$fieldConfiguration = empty($options['fieldConfiguration']) ? array() : $options['fieldConfiguration'];
+
+			$columns = array(
+				$fieldName => array(
+					'exclude' => $exclude,
+					'label' => $label,
+					'config' => static::getTcaFieldConfiguration($tableName, $fieldName, $fieldConfiguration),
+				),
+			);
+
+			// Adding fields to an existing table definition
+			ExtensionManagementUtility::addTCAcolumns($tableName, $columns);
+		}
+	}
+
+	/**
+	 * Creates the 'fieldList' string for $fieldName which includes a categories tab.
+	 * But only one categories tab is added per table.
+	 *
+	 * @param string $tableName
+	 * @param string $fieldName
+	 * @return string
+	 */
+	protected function addDataQualityTab($tableName, $fieldName) {
+		$fieldList = '';
+		if (!in_array($tableName, $this->addedDataQualityTabs)) {
+			$fieldList .= '--div--;LLL:EXT:duplicate_finder/Resources/Private/Language/locallang_tca.xlf:label.tab.dataQuality, ';
+			$this->addedDataQualityTabs[] = $tableName;
+		}
+		$fieldList .= $fieldName;
+		return $fieldList;
+	}
+
+	/**
+	 * Add a new field into the TCA types -> showitem
+	 *
+	 * @param string $tableName Name of the table to be categorized
+	 * @param string $fieldName Name of the field to be used to store categories
+	 * @param array $options Additional configuration options
+	 *              + fieldList: field configuration to be added to showitems
+	 *              + typesList: list of types that shall visualize the categories field
+	 *              + position: insert position of the categories field
+	 * @return void
+	 */
+	protected function addToAllTcaTypes($tableName, $fieldName, array $options) {
+
+		// Makes sure to add more TCA to an existing structure
+		if (isset($GLOBALS['TCA'][$tableName]['columns'])) {
+
+			if (empty($options['fieldList'])) {
+				$fieldList = $this->addDataQualityTab($tableName, $fieldName);
+			} else {
+				$fieldList = $options['fieldList'];
+			}
+
+			$typesList = '';
+			if (!empty($options['typesList'])) {
+				$typesList = $options['typesList'];
+			}
+
+			$position = '';
+			if (!empty($options['position'])) {
+				$position = $options['position'];
+			}
+
+			// Makes the new "categories" field to be visible in TSFE.
+			ExtensionManagementUtility::addToAllTCAtypes($tableName, $fieldList, $typesList, $position);
+
+		}
+	}
+
+	/**
+	 * Get the config array for given table and field.
+	 * This method does NOT take care of adding sql fields nor adding the field to TCA types
+	 * This has to be taken care of manually!
+	 *
+	 * @param string $tableName The table name
+	 * @param string $fieldName The field name (default isDuplicate)
+	 * @param array $fieldConfigurationOverride Changes to the default configuration
+	 * @return array
+	 * @api
+	 */
+	static public function getTcaFieldConfiguration($tableName, $fieldName = 'isDuplicate', array $fieldConfigurationOverride = array()) {
+		// set up a new field, default name is 'isDuplicate'
+		$fieldConfiguration = array(
+			'config' => array(
+				'type' => 'user',
+				'userFunc' => 'CPSIT\DuplicateFinder\Service\Tca\DuplicateConfigurationService->getIsDuplicateField',
+				'parameters' => array(
+					'tableName' => $tableName,
+					'fieldName' => $fieldName,
+					'fieldConfigurationOverride' => $fieldConfigurationOverride,
+				)
+			),
+		);
+
+		// Merge changes to TCA configuration
+		if (!empty($fieldConfigurationOverride)) {
+			ArrayUtility::mergeRecursiveWithOverrule(
+				$fieldConfiguration,
+				$fieldConfigurationOverride
+			);
+		}
+
+		return $fieldConfiguration;
+	}
+}

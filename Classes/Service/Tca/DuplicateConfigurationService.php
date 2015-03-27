3fdfd17d9c0e7372f71989ddea402cbfa40cbb04
Dirk Wenzel <wenzel@cps-it.de>
2015-03-27 09:35:46 +0100

    [TASK] DuplicateConfigurationService now renders a field with duplicate status, and either the original record with an show link or a list of duplicates or only a message.

--- Classes/Service/Tca/DuplicateConfigurationService.php
+++ Classes/Service/Tca/DuplicateConfigurationService.php
@@ -12,7 +12,9 @@ namespace CPSIT\DuplicateFinder\Service\Tca;
  *
  * The TYPO3 project - inspiring people to share!
  */
+use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Backend\Utility\IconUtility;
+
 /**
  * Class DuplicateConfigurationService
  * Provides TCA configuration for records implementing the DuplicateInterface
@@ -23,6 +25,56 @@ use TYPO3\CMS\Backend\Utility\IconUtility;
  */
 class DuplicateConfigurationService {
 	/**
+	 * @var string
+	 */
+	protected static $languageFile = 'LLL:EXT:duplicate_finder/Resources/Private/Language/locallang.xlf:';
+
+	/**
+	 * @var string
+	 */
+	protected static $fieldTemplateIsDuplicate = '<strong>###FIELD_NAME###</strong><br />
+					<p>###FIELD_INTRO###<br />
+					<span class="status ###FIELD_STATUSCLASS###">###FIELD_STATUS###</span> ###FIELD_DUPLICATEMESSAGE###</p>
+					###FIELD_RECORDS###';
+
+	/**
+	 * @var array
+	 */
+	protected $templateMarker = array('NAME', 'STATUSCLASS', 'STATUS', 'DUPLICATEMESSAGE', 'RECORDS');
+
+	/**
+	 * @var string
+	 */
+	protected static $fieldWrap = '<tr class="class-main1"><td colspan="2" class="formField-field" valign="top">
+				<span class="t3-form-field-container"><div class="t3-form-field-item tx-duplicate-finder">|</div></span></td></tr>';
+
+	/**
+	 * @var string
+	 */
+	protected static $duplicateListEntryTemplate = '<li>###FIELD_EDITLINK###<span class="title">###FIELD_TITLE###</span></li>';
+
+	/**
+	 * @var string
+	 */
+	protected static $originalRecordTemplate = '<div>###FIELD_SHOWLINK###<span class="title">###FIELD_TITLE###</span>';
+
+	/**
+	 * @var string
+	 */
+	protected static $editLinkTemplate = '<a class="btn edit"
+				href="alt_doc.php?edit[###FIELD_TABLE###][###FIELD_UID###]=edit&returnUrl=###FIELD_RETURNURL###"
+				title="###FIELD_TITLE###">###FIELD_ICON###</a>';
+
+
+	/**
+	 * @var string
+	 */
+	protected static $showLinkTemplate = '<a class="btn show"
+				href="#"
+				onclick="top.launchView(\'###FIELD_TABLE###\', \'###FIELD_UID###\'); return false;"
+				title="###FIELD_TITLE###">###FIELD_ICON###</a>';
+
+	/**
 	 * User function: Gets a backend form field for 'is_duplicate'
 	 *
 	 * @param \array $parameters
@@ -30,16 +82,164 @@ class DuplicateConfigurationService {
 	 * @return \string|NULL
 	 */
 	public function getIsDuplicateField ($parameters, $parentObject) {
-		$field = NULL;
-		if ((bool)$parameters['row']['is_duplicate']) {
-			$field = $parentObject->getSingleHiddenField(
-				$parameters['table'],
-				$parameters['field'],
-				$parameters['row'],
-				$parameters
-			);
-			$field .= IconUtility::getSpriteIcon('extensions-duplicate_finder-icon-is-duplicate');
+		$isDuplicate = (bool)$parameters['row'][$parameters['field']];
+		$isHashed = (bool)($parameters['row']['duplicate_hash_id']);
+		$this->templateMarker['NAME'] = $parameters['label'];
+		$this->templateMarker['INTRO'] = $parentObject->sL(self::$languageFile . 'question.duplicateTestPassed');
+		$field = $parentObject->getSingleHiddenField(
+			$parameters['table'],
+			$parameters['field'],
+			$parameters['row'],
+			$parameters
+		);
+		if ($isHashed) {
+			if ($isDuplicate) {
+				$labelKey = 'label.failed';
+				$this->templateMarker['DUPLICATEMESSAGE'] = $parentObject->sL(self::$languageFile . 'message.isDuplicateOf');
+				$this->templateMarker['STATUSCLASS'] = 'error';
+				$this->templateMarker['RECORDS'] = $this->renderOriginal($parameters['row'], $parameters['table'], $parentObject);
+			} else {
+				$labelKey = 'label.passed';
+				$this->templateMarker['STATUSCLASS'] = 'ok';
+				$duplicateRecords = $this->getDuplicates($parameters['table'], $parameters['field'], $parameters['row']);
+				if ((bool) $duplicateRecords) {
+					$this->templateMarker['RECORDS'] = $this->renderDuplicatesList($duplicateRecords, $parameters['table'], $parentObject);
+					$this->templateMarker['DUPLICATEMESSAGE'] = sprintf(
+						$parentObject->sL(self::$languageFile . 'message.hasDuplicates'),
+						count($duplicateRecords)
+					);
+				} else {
+					$this->templateMarker['DUPLICATEMESSAGE'] = $parentObject->sL(self::$languageFile . 'message.noDuplicates');
+				}
+			}
+		} else {
+			$labelKey = 'label.notProcessedYet';
+			$this->templateMarker['STATUSCLASS'] = 'notice';
 		}
-		return $field;
+		$this->templateMarker['STATUS'] = $parentObject->sL(self::$languageFile . $labelKey);
+
+		$field .= $parentObject->intoTemplate($this->templateMarker, self::$fieldTemplateIsDuplicate);
+		$parts = explode('|', self::$fieldWrap, 2);
+		$out = $parts[0] . $field . $parts[1];
+		return $out;
+	}
+
+	/**
+	 * Gets duplicates for an original record
+	 *
+	 * @param \string $tableName
+	 * @param \string $fieldName
+	 * @param \array $originalRecord
+	 * @return \array | NULL
+	 */
+	protected function getDuplicates ($tableName, $fieldName, $originalRecord) {
+		return BackendUtility::getRecordsByField(
+			$tableName,
+			$fieldName,
+			'1',
+			' AND duplicate_hash_id=' . $originalRecord['duplicate_hash_id']
+		);
+	}
+
+	/**
+	 * Get the original for a duplicate
+	 *
+	 * @param \string $tableName
+	 * @param \string $fieldName
+	 * @param \array $duplicateRecord
+	 * @return \array | NULL
+	 */
+	protected function getOriginal ($tableName, $fieldName, $duplicateRecord) {
+		return BackendUtility::getRecordsByField(
+			$tableName,
+			$fieldName,
+			'0',
+			' AND duplicate_hash_id=' . $duplicateRecord['duplicate_hash_id'],
+			'',
+			'',
+			1
+		);
+	}
+
+	/**
+	 * Renders a list of duplicates with edit link buttons
+	 * 
+	 * @param \array $duplicates An array of records
+	 * @param \string $tableName
+	 * @param \TYPO3\CMS\Backend\Form\FormEngine $parentObject
+	 * @return \string A list of duplicates
+	 */
+	protected function renderDuplicatesList ($duplicates, $tableName, &$parentObject) {
+		$duplicatesList = '<ul>';
+		foreach ($duplicates as $record) {
+			$duplicatesList .= $parentObject->intoTemplate(
+				array(
+					'TITLE' => BackendUtility::getRecordTitle($tableName, $record, TRUE, TRUE),
+					'EDITLINK' => $this->renderEditLink($record, $tableName, $parentObject)
+				),
+				self::$duplicateListEntryTemplate
+			);
+		}
+		$duplicatesList .= '</ul>';
+		return $duplicatesList;
+	}
+
+	/**
+	 * Renders the title of the original with a show link
+	 *
+	 * @param \array $record An array with values
+	 * @param \string $tableName
+	 * @param \TYPO3\CMS\Backend\Form\FormEngine $parentObject
+	 * @return \string HTML
+	 */
+	protected function renderOriginal ($record, $tableName, &$parentObject) {
+		return $parentObject->intoTemplate(
+			array(
+				'TITLE' => BackendUtility::getRecordTitle($tableName, $record, TRUE, TRUE),
+				'SHOWLINK' => $this->renderShowLink($record, $tableName, $parentObject),
+			),
+			self::$originalRecordTemplate
+		);
+	}
+
+	/**
+	 * Renders an edit link with icon for a record.
+	 *
+	 * @param \array $record An array with values
+	 * @param \string $tableName
+	 * @param \TYPO3\CMS\Backend\Form\FormEngine $parentObject
+	 * @return \string An edit wizard
+	 */
+	protected function renderEditLink ($record, $tableName, $parentObject) {
+		return $parentObject->intoTemplate(
+			array(
+				'TITLE' => $parentObject->sl(self::$languageFile . 'label.edit'),
+				'TABLE' => $tableName,
+				'UID' => $record['uid'],
+				'ICON' => IconUtility::getSpriteIcon('actions-document-open'),
+				'RETURNURL' => rawurlencode($parentObject->thisReturnUrl()),
+			),
+			self::$editLinkTemplate
+		);
+	}
+
+	/**
+	 * Renders a show link with icon for a record
+	 *
+	 * @param \array $record An array with values
+	 * @param \string $tableName
+	 * @param \TYPO3\CMS\Backend\Form\FormEngine $parentObject
+	 * @return \string An edit wizard
+	 */
+	protected function renderShowLink ($record, $tableName, $parentObject) {
+		return $parentObject->intoTemplate(
+			array(
+				'TITLE' => $parentObject->sl(self::$languageFile . 'label.show'),
+				'TABLE' => $tableName,
+				'UID' => $record['uid'],
+				'ICON' => IconUtility::getSpriteIcon('actions-document-info'),
+			),
+			self::$showLinkTemplate
+		);
 	}
 }
